230799882521182c9b12030d1fcbf9e22911f18f
ovidiuusva <150781665+ovidiuusva@users.noreply.github.com>
2024-06-11 09:00:07 +0300

    first commit

---
+++ lib/presentation/pages/counter/bloc/counter_bloc.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterInitial()) {
+    on<CounterEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
